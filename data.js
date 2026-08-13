ed2190e4efbb3a7260cfc14753583b88436125f3
Elsayed Anwar <222307882+elsayedesm@users.noreply.github.com>
2026-08-13 12:43:33 +0300

    Add statistics data to the stats array

--- data.js
+++ data.js
@@ -16,7 +16,12 @@ export const initialData = {
       primaryCta: "استعرض أعمالنا",
       secondaryCta: "احجز استشارة مجانية"
     },
-    stats: []
+    stats: [
+      { id: "s1", number: "+150", labelEn: "Campaigns Launched", labelAr: "حملة تسويقية ناجحة" },
+      { id: "s2", number: "+380%", labelEn: "Average ROI Boost", labelAr: "متوسط زيادة العائد" },
+      { id: "s3", number: "25M+", labelEn: "Total Ad Reach", labelAr: "وصول الإعلانات" },
+      { id: "s4", number: "18", labelEn: "Industry Awards", labelAr: "جائزة إبداعية" }
+    ]
   },
 
   about: {
